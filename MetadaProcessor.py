47a8ef3fcfa177d412b19e76be231b19797a5238
Rana Coskun <coskunrana13@gmail.com>
2023-05-18 20:10:12 +0200

    modify collection Processor

--- MetadaProcessor.py
+++ MetadaProcessor.py
@@ -2,13 +2,11 @@ from sqlite3 import connect
 from pandas import read_csv, Series, DataFrame
 import pandas as pd
 
-metadata = read_csv("C:\\Users\\chiar\\Documents\\Magistrale\\DataScience\\metadata.csv", 
-                  keep_default_na=False,
-                  dtype={
-                      "id": "string",
-                      "title": "string",
-                      "creator": "string"
-                  })
+metadata = read_csv(
+    "C:\\Users\\chiar\\Documents\\Magistrale\\DataScience\\metadata.csv",
+    keep_default_na=False,
+    dtype={"id": "string", "title": "string", "creator": "string"},
+)
 
 # 'ITEMS': RELATION COLLECTION-MANIFEST
 collection_manifest = pd.DataFrame()
@@ -17,67 +15,81 @@ collection_manifest = pd.DataFrame()
 manifest_canvas = pd.DataFrame()
 
 # COLLECTIONS TABLE
-collections = pd.DataFrame() # empty DataFrame object
+collections = pd.DataFrame()  # empty DataFrame object
 collections_id = []
 collections_title = []
 for column_name, column in metadata.items():
     for value in column:
         if "collection" in value:
-            collections_id.append(value) # Iterate over the values of the columns and I append in a list only the ones with the word "collection" in it, which are the IDs of the collections 
+            collections_id.append(
+                value
+            )  # Iterate over the values of the columns and I append in a list only the ones with the word "collection" in it, which are the IDs of the collections
             # (to be improved: find a way to iterate only over column 'id')
-            title = metadata.loc[metadata["id"] == value, "title"].values[0] # Select the rows where the value of the column "id" is equal to my variable "value" (i.e. in this case the id of the collection), then I select the value in the same row but in the column "title" (i.e. the title corresponding to the collection I found)
+            title = metadata.loc[metadata["id"] == value, "title"].values[
+                0
+            ]  # Select the rows where the value of the column "id" is equal to my variable "value" (i.e. in this case the id of the collection), then I select the value in the same row but in the column "title" (i.e. the title corresponding to the collection I found)
             collections_title.append(title)
 
-collections.insert(0, "id", Series(collections_id, dtype="string")) # Append the items in my list in a column of the DataFrame "collections"
+collections.insert(
+    0, "id", Series(collections_id, dtype="string")
+)  # Append the items in my list in a column of the DataFrame "collections"
 collections.insert(1, "title", Series(collections_title, dtype="string"))
 
 collections_internal_id = []
 for idx, row in collections.iterrows():
-    collections_internal_id.append("collection-" + str(idx)) # Create a list of internal ids
+    collections_internal_id.append(
+        "collection-" + str(idx)
+    )  # Create a list of internal ids
 
-collections.insert(0, "manifestId", Series(collections_internal_id, dtype="string")) # Append the internal IDs in a column of the Data Frame "collections"
+collections.insert(
+    0, "manifestId", Series(collections_internal_id, dtype="string")
+)  # Append the internal IDs in a column of the Data Frame "collections"
 
 # MANIFESTS TABLE
-manifests = pd.DataFrame() # empty DataFrame object
+manifests = pd.DataFrame()  # empty DataFrame object
 manifests_id = []
 manifests_title = []
 for column_name, column in metadata.items():
     for value in column:
         if "manifest" in value:
-            manifests_id.append(value)  
+            manifests_id.append(value)
             title = metadata.loc[metadata["id"] == value, "title"].values[0]
             manifests_title.append(title)
 
-manifests.insert(0, "id", Series(manifests_id, dtype="string")) # Append the items in my list in a column of the DataFrame "manifests"
+manifests.insert(
+    0, "id", Series(manifests_id, dtype="string")
+)  # Append the items in my list in a column of the DataFrame "manifests"
 manifests.insert(1, "title", Series(manifests_title, dtype="string"))
 
 manifests_internal_id = []
 for idx, row in manifests.iterrows():
-    manifests_internal_id.append("manifest-" + str(idx)) # Create a list of internal ids
+    manifests_internal_id.append(
+        "manifest-" + str(idx)
+    )  # Create a list of internal ids
 
-manifests.insert(0, "manifestId", Series(manifests_internal_id, dtype="string")) # Append the internal IDs in a column of the Data Frame "manifests"
+manifests.insert(
+    0, "manifestId", Series(manifests_internal_id, dtype="string")
+)  # Append the internal IDs in a column of the Data Frame "manifests"
 
 # CANVAS TABLE
-canvases = pd.DataFrame() 
+canvases = pd.DataFrame()
 canvases_id = []
 canvases_title = []
 for column_name, column in metadata.items():
     for value in column:
         if "canvas" in value:
             canvases_id.append(value)
-            title = metadata.loc[metadata["id"] == value, "title"].values[0] 
+            title = metadata.loc[metadata["id"] == value, "title"].values[0]
             canvases_title.append(title)
 
-canvases.insert(0, "id", Series(canvases_id, dtype="string")) 
+canvases.insert(0, "id", Series(canvases_id, dtype="string"))
 canvases.insert(1, "title", Series(canvases_title, dtype="string"))
 
 canvases_internal_id = []
 for idx, row in canvases.iterrows():
-     canvases_internal_id.append("canvas-" + str(idx))
+    canvases_internal_id.append("canvas-" + str(idx))
 
 canvases.insert(0, "canvasesId", Series(canvases_internal_id, dtype="string"))
-
-
 
 
 # print(collections)
@@ -85,7 +97,6 @@ canvases.insert(0, "canvasesId", Series(canvases_internal_id, dtype="string"))
 # print(canvases)
 
 # with connect("relationaldatabase.db") as con:
-    
 
 
 #     con.commit()
@@ -94,4 +105,3 @@ canvases.insert(0, "canvasesId", Series(canvases_internal_id, dtype="string"))
 # class MetadataProcessor(object): #the superclass should be Processor
 #     def super().__init__(dbPathOrUrl)
 #     def uploadData(self, path):
-
--- collectionProcessor.py
+++ collectionProcessor.py
@@ -5,32 +5,36 @@ from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 
 
 # classes of resources
-EntitywithMetadata = URIRef('')
-Collection = URIRef('')
-Manifest = URIRef('')
-Canvas = URIRef('')
-#why not adding also
-IdentifiableEntity = URIRef('')
-Annotation = URIRef('')
-Image = URIRef('')
+EntitywithMetadata = URIRef("")
+Collection = URIRef("")
+Manifest = URIRef("")
+Canvas = URIRef("")
+# why not adding also
+IdentifiableEntity = URIRef("")
+Annotation = URIRef("")
+Image = URIRef("")
 
 
 # attributes related to classes
-# attributes in json file or in UML ???? >> I think in the UML 
-id = URIRef('')
-type = URIRef('') #why this since it the json it addresses the class to which the item belongs?
-label = URIRef('')
-item = URIRef('') #i think this is viewed as a relation within the collection (see point below), so it's not an attribute (???)
-title = URIRef('')
+# attributes in json file or in UML ???? >> I think in the UML
+id = URIRef("")
+type = URIRef(
+    ""
+)  # why this since it the json it addresses the class to which the item belongs?
+label = URIRef("")
+item = URIRef(
+    ""
+)  # i think this is viewed as a relation within the collection (see point below), so it's not an attribute (???)
+title = URIRef("")
 # probably these two are totally useless but let's keep them here??
 # creators = URIRef('')
 # motivation = URIRef('')
 
-# I think these are also to be added: 
+# I think these are also to be added:
 # relations among classes
-target = URIRef('')
-body = URIRef('')
-items = URIRef('')
+target = URIRef("")
+body = URIRef("")
+items = URIRef("")
 
 
 class CollectionProcessor(Processor):
@@ -40,9 +44,9 @@ class CollectionProcessor(Processor):
         # self.dbPathOrUrl = None
 
     def uploadData(self, path):
-        graph = create_triples(self, path) 
+        graph = create_triples(self, path)
         store = SPARQLUpdateStore()
-        endpoint = ''  # database path == self.dbPathOrUrl
+        endpoint = ""  # database path == self.dbPathOrUrl
 
         store.open((endpoint, endpoint))
 
@@ -57,35 +61,34 @@ class CollectionProcessor(Processor):
         with open(path, "r", encoding="utf-8") as f:
             json_doc = load(f)
 
-            for name, value in json_doc.items(): 
-                if name == 'id':
+            for name, value in json_doc.items():
+                if name == "id":
                     subject = URIRef(value)
 
-                if name == 'type':
+                if name == "type":
                     # should we add collection case(???)
-                    if value == 'Collection':  # json.dump() ???
-                        object = Collection 
+                    if value == "Collection":  # json.dump() ???
+                        object = Collection
 
-                    if value == 'Manifest':  # json.dump() ???
+                    if value == "Manifest":  # json.dump() ???
                         object = Manifest
 
-                    if value == 'Canvas':  # json.dump() ???
+                    if value == "Canvas":  # json.dump() ???
                         object = Canvas
 
                     triple = (subject, RDF.type, object)
                     graph.add(triple)
 
-                if name == 'label':
+                if name == "label":
                     for name_l, value_l in value.items():  # might not work
                         # subject = name_l
                         object = Literal(value_l)
                         triple = (subject, label, object)
                         graph.add(triple)
 
-                if name == 'item': #i think it's with an "s" >> items
-                    for d in value:  
+                if name == "item":  # i think it's with an "s" >> items
+                    for d in value:
                         # another triple for item???
-                        create_triples(graph, d) #is d a path? I am confused
-                        # were you using json.dump so that the file "empties" itself 
+                        create_triples(graph, d)  # is d a path? I am confused
+                        # were you using json.dump so that the file "empties" itself
                         # and one can go back to its new version recursively ??
-
